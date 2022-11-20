89bdef9f98ebc5465c61d37790fed99d608f7318
Jacques kibamba <jacqueskibamba@gmail.com>
2022-11-20 23:36:43 +0300

    added currency enum

---
+++ src/main/java/ru/rsreu/kibamba/Main.java
@@ -0,0 +1,7 @@
+package ru.rsreu.kibamba;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/ru/rsreu/kibamba/model/Currency.java
@@ -0,0 +1,18 @@
+package ru.rsreu.kibamba.model;
+
+public enum Currency {
+    //Chinese Yuan
+    CNY,
+    //United States Dollar
+    USD,
+    //Canadian Dollar
+    CAD,
+    //Euro
+    EUR,
+    //Russian Ruble
+    RUB,
+    //Australian Dollars
+    AUD,
+    //Turkish Lira
+    TRY
+}
