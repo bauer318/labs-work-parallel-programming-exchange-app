1f8b5f2a10561aa2ffdcfa3f3215433e39c485f0
Jacques kibamba <jacqueskibamba@gmail.com>
2022-12-05 18:18:55 +0300

    empty here

--- src/main/java/ru/rsreu/kibamba/logic/CurrencyWorker.java
+++
@@ -1,8 +0,0 @@
-package ru.rsreu.kibamba.logic;
-
-import java.math.RoundingMode;
-
-public class CurrencyWorker {
-    public static final RoundingMode CURRENCY_ROUNDING_MODE = RoundingMode.HALF_EVEN;
-    public static final int CURRENCY_SCALE=2;
-}
--- src/main/java/ru/rsreu/kibamba/logic/OrderType.java
+++
@@ -1,6 +0,0 @@
-package ru.rsreu.kibamba.logic;
-
-public enum OrderType {
-    SELL,
-    BUY
-}
